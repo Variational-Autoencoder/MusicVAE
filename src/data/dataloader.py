083835c9e58238fbca2eabe8ff741babb7c5e33e
Kevin Broløs <kevin.broloes@gmail.com>
2018-11-27 21:32:14 +0100

    Use smaller types for the one hot encoded columns in the data loader to avoid wasting memory

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -1,21 +1,35 @@
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
+import pretty_midi
 
 class MidiDataset(Dataset):
     """Pre-processed MIDI dataset."""
 
-    def __init__(self, csv_file, transform=None):
+    def __init__(self, csv_file, transform=None, midi_start=48, midi_end=108):
         """
         Args:
             csv_file (string): Path to the csv file with piano rolls per song.
             transform (callable, optional): Optional transform to be applied
                 on a sample.
+            midi_start (int): The first midi note in the dataset
+            midi_end (int): The last midi note in the dataset
         """
-        self.piano_rolls = pd.read_csv(csv_file, sep=';', index_col=['piano_roll_name', 'timestep'])
+        dtypes = {'piano_roll_name': 'object', 'timestep': 'uint32'}
+        column_names = [pretty_midi.note_number_to_name(n) for n in range(midi_start, midi_end)]
+        for column in column_names:
+            dtypes[column] = 'uint8'
+
+        self.piano_rolls = pd.read_csv(csv_file, sep=';', index_col=['piano_roll_name', 'timestep'], dtype=dtypes)
         self.transform = transform
 
     def __len__(self):
         return len(self.piano_rolls.index.levels[0])
+
+    def get_mem_usage(self):
+        """
+            Returns the memory usage in MB
+        """
+        return self.piano_rolls.memory_usage(deep=True).sum() / 1024**2
 
     def get_indexer(self):
         """
