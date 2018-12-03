ff0f001682e9bebb7325a51b09039715bbffef34
Kevin Broløs <kevin.broloes@gmail.com>
2018-12-03 15:51:18 +0100

    Turn the transformer into a callable class instead (might fix an issue on a windows conda install)

--- src/data/bar_transform.py
+++ src/data/bar_transform.py
@@ -1,25 +1,26 @@
 import numpy as np
 import math
 
-def BarTransform(bars=1, note_count=60):
-    split_size = bars*16
+class BarTransform():
 
-    def transform(sample):
+    def __init__(self, bars=1, note_count=60):
+        self.split_size = bars*16
+        self.note_count = note_count
+
+    def __call__(self, sample):
         sample_length = len(sample)
 
         # Pad the sample with 0's if there's not enough to create equal splits into n bars
-        leftover = sample_length % split_size
+        leftover = sample_length % self.split_size
         if leftover != 0:
-            padding_size = split_size - leftover
-            padding = np.zeros((padding_size, note_count))
+            padding_size = self.split_size - leftover
+            padding = np.zeros((padding_size, self.note_count))
             sample = np.append(sample, padding, axis=0)
 
 
-        sections = math.ceil(sample_length / split_size)
+        sections = math.ceil(sample_length / self.split_size)
         # Split into X equal sections
         split_list = np.array_split(sample, indices_or_sections=sections)
 
 
         return split_list
-
-    return transform
